854964d23f270fe5a215cdd73bb22064dd828ac8
Xinyu Zhou <zxytim@gmail.com>
2013-12-10 18:20:55 +0800

    add commandline support

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,6 @@
 /*
  * $File: main.cc
- * $Date: Tue Dec 10 16:43:56 2013 +0800
+ * $Date: Tue Dec 10 18:20:51 2013 +0800
  * $Author: Xinyu Zhou <zxytim[at]gmail[dot]com>
  */
 
@@ -19,16 +19,6 @@ using namespace TCLAP;
 
 typedef std::vector<std::vector<real_t>> DenseDataset;
 
-static void svm2vec(const std::vector<std::pair<int, real_t>> &input,
-		std::vector<real_t> &output, int dim) {
-	output = std::vector<real_t>(dim, 0);
-	for (auto &item: input) {
-		if (item.first > dim) // ignore out bounded values
-			continue;
-		output[item.first] = item.second;
-	}
-}
-
 vector<real_t> string_to_double_vector(string line) {
 	vector<real_t> x;
 	int begin = 0, end = 0;
@@ -45,23 +35,14 @@ vector<real_t> string_to_double_vector(string line) {
 	return x;
 }
 
-void Dataset2DenseDataset(Dataset &X0, DenseDataset &X1) {
-	int n, m;
-	get_data_metric(X0, n, m);
-
-	X1.resize(X0.size());
-	for (size_t i = 0; i < X0.size(); i ++)
-		svm2vec(X0[i], X1[i], m);
-}
-
-
-
 struct Args {
 	int concurrency;
 	int K;
+	int iteration;
+	real_t min_covar = 1e-3;
 
 	string input_file;
-	string output_file;
+	string model_file;
 };
 
 Args parse_args(int argc, char *argv[]) {
@@ -71,22 +52,30 @@ Args parse_args(int argc, char *argv[]) {
 
 		ValueArg<int> arg_concurrency("w", "concurrency", "number of workers", false, 1, "NUMBER");
 		ValueArg<int> arg_K("k", "K", "number of gaussians", true, 10, "NUMBER");
+		ValueArg<double> arg_min_covar("c", "mincovar", "minimum covariance to avoid overfitting, default 1e-3.", false, 1e-3, "FLOAT");
 
 		ValueArg<string> arg_input_file("i", "input", "intput file", true, "", "FILE");
-		ValueArg<string> arg_output_file("o", "output", "intput file", true, "", "FILE");
+		ValueArg<string> arg_model_file("m", "model", "model file", true, "", "FILE");
+		ValueArg<int> arg_iteration("r", "iteration", "number of iterations",
+				false, 200, "NUMBER");
 
 		cmd.add(arg_concurrency);
 		cmd.add(arg_K);
+		cmd.add(arg_min_covar);
 		cmd.add(arg_input_file);
-		cmd.add(arg_output_file);
+		cmd.add(arg_model_file);
+		cmd.add(arg_iteration);
+
 
 		cmd.parse(argc, argv);
 
 #define GET_VALUE(name) args.name = arg_##name.getValue();
 		GET_VALUE(concurrency);
 		GET_VALUE(K);
+		GET_VALUE(min_covar);
 		GET_VALUE(input_file);
-		GET_VALUE(output_file);
+		GET_VALUE(model_file);
+		GET_VALUE(iteration);
 
 	} catch (ArgException &e) {
 		cerr << "error: " << e.error() << " for arg " << e.argId() << endl;
@@ -111,67 +100,19 @@ void write_dense_dataset(DenseDataset &X, const char *fname) {
 	}
 }
 
-void fill_gaussian(DenseDataset &X, Gaussian *gaussian, int nr_point) {
-	for (int i = 0; i < nr_point; i ++)
-		X.push_back(gaussian->sample());
-}
-
-static vector<real_t> random_vector(int dim, real_t range, Random &random) {
-	vector<real_t> vec(dim);
-	for (auto &v: vec) v = random.rand_real() * range;
-	return vec;
-}
-
-void gen_high_dim_gaussian_mixture(DenseDataset &X, int dim, int nr_gaussian, int nr_point_per_gaussian) {
-	Random random;
-	for (int i = 0; i < nr_gaussian; i ++) {
-		Gaussian g(dim);
-		g.mean = random_vector(dim, 1, random);
-		g.sigma = random_vector(dim, 0.05 + random.rand_real() * 0.1, random);
-		fill_gaussian(X, &g, nr_point_per_gaussian);
-	}
-}
-
-void gen_gaussian_mixture(DenseDataset &X, int nr_point_per_gaussian = 1000) {
-	int nr_gaussian = 3;
-	Gaussian g0(2);
-	g0.mean = {0, 0};
-	g0.sigma = {0.1, 0.1};
-
-	Gaussian g1(2);
-	g1.mean = {1, 1};
-	g1.sigma = {0.1, 0.1};
-
-	Gaussian g2(2);
-	g2.mean = {2, 1};
-	g2.sigma = {0.2, 0.2};
-
-	fill_gaussian(X, &g0, nr_point_per_gaussian);
-	fill_gaussian(X, &g1, nr_point_per_gaussian);
-	fill_gaussian(X, &g2, nr_point_per_gaussian);
-}
-
 int main(int argc, char *argv[]) {
-	srand(42); // Answer to The Ultimate Question of Life, the Universe, and Everything
-//    Args args = parse_args(argc, argv);
+//    srand(42); // Answer to The Ultimate Question of Life, the Universe, and Everything
+	Args args = parse_args(argc, argv);
 
 	DenseDataset X;
-//    read_dense_dataset(X, "test.data");
-	gen_gaussian_mixture(X, 100);
-//    gen_high_dim_gaussian_mixture(X, 13, 10, 68000);
+	read_dense_dataset(X, args.input_file.c_str());
 
-	write_dense_dataset(X, "test.data");
-
-
-	int concurrency = 4;
-	int nr_mixture = 3;
-
-	GMMTrainerBaseline trainer(1, 1e-3, concurrency);
-	GMM gmm(nr_mixture, COVTYPE_DIAGONAL, &trainer);
+	GMMTrainerBaseline trainer(args.iteration, args.min_covar, args.concurrency);
+	GMM gmm(args.K, COVTYPE_DIAGONAL, &trainer);
 	printf("start training ...\n"); fflush(stdout);
 	gmm.fit(X);
 
-	ofstream fout("gmm-test.model");
+	ofstream fout(args.model_file);
 	gmm.dump(fout);
 
 	return 0;
